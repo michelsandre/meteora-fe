b06c73d201974bbf08479a0bd3961f80ce360bf1
Andre Michels de Oliveira <michelsandre@gmail.com>
2025-03-17 12:57:23 -0300

    feat: :construction_worker: em andamento

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,9 @@
+import banner from '../../assets/banner.png';
+
+export const Banner = () => {
+  return (
+    <div className="banner-container">
+      <img src={banner} alt="Banner image" />
+    </div>
+  );
+};
---
+++ src/components/Categoria/index.tsx
@@ -0,0 +1,11 @@
+export const Categoria = () => {
+  return (
+    <section>
+      <h3>Busque por categoria:</h3>
+      <div>
+        <img src="" alt="" />
+        <p>Camisetas</p>
+      </div>
+    </section>
+  );
+};
